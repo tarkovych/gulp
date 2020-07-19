ed1a5b3392eaf6ac20e1d86fc2d165b82be9e8dc
tarkovych <tarkovychne2@gmail.com>
2020-07-19 17:01:54 +0300

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var less = require('gulp-less');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var cssmin = require('gulp-cssmin');
+var autoprefixer = require('gulp-autoprefixer');
 function css(done){
   gulp.src("./less/*.less")
   .pipe(less({ 
@@ -11,11 +12,11 @@ function css(done){
     outputStyle:'compressed'
   }))
   .on('error',console.error.bind(console))
+  .pipe(autoprefixer({overrideBrowserslist: ['last 2 versions'],cascade: false}))
   .pipe(gulp.dest('./css/'))
   .pipe(cssmin().on('error', function(err) {console.log('error-css-min',err)}))
   .pipe(rename({suffix:'.min'}))
   .pipe(gulp.dest('./css-min/'))
-
 
   gulp.src("./scss/*.scss")
   .pipe(sass({
@@ -23,6 +24,7 @@ function css(done){
     outputStyle:'compressed'
   }))
   .on('error',console.error.bind(console))
+  .pipe(cssmin().on('error', function(err) {console.log('error-css-min',err)}))
   .pipe(gulp.dest('./css/'))
   .pipe(cssmin().on('error', function(err) {console.log('error-css-min',err)}))
   .pipe(rename({suffix:'.min'}))
