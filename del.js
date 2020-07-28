cd724b92e74e242709f942a02958e78615d2dc90
tarkovych <tarkovychne2@gmail.com>
2020-07-28 11:24:20 +0300

    node

--- del.js
+++ del.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 
-fs.unlink('/Users/mac/Library/Preferences/SmartGit/19.1/preferences.yml', function(err) {
+fs.unlink('/Users/mac/Library/Preferences/SmartGit/20.1/preferences.yml', function(err) {
   if (err) throw err;
-
   console.log('file deleted');
 });
-fs.unlink('/Users/mac/Library/Preferences/SmartGit/19.1/.backup/preferences.yml', function(err) {
+fs.unlink('/Users/mac/Library/Preferences/SmartGit/20.1/license', function(err) {
+  if (err) throw err;
+  console.log('file deleted');
+});
+fs.unlink('/Users/mac/Library/Preferences/SmartGit/20.1/.backup/preferences.yml', function(err) {
   if (err) throw err;
 
   console.log('file deleted');
